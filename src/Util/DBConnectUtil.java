028913c8cbcbb1702c363d4b371c933b259f41df
Nathan Devroy <nathan.devroy@optum.com>
2018-01-24 10:27:38 -0600

    Integrate new functionality for device processing

--- src/Util/DBConnectUtil.java
+++ src/Util/DBConnectUtil.java
@@ -3,10 +3,6 @@ package Util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-
-import packet_fields.Packet;
-import packet_fields.Impl.PacketImpl;
 
 public class DBConnectUtil {
 
